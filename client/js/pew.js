635d50484b48791996474c7b7e40605019d1381e
Ingrid <hi@ingridcheung.com>
2013-01-04 17:00:21 -0500

    Test browser client.

---
+++ client/js/pew.js
@@ -0,0 +1,28 @@
+window.onload = function(){
+    // Variables.
+    var conn, game, ws;
+
+    // Functions.
+    var openConnection;
+
+    if (window.MozWebSocket) {
+        // Firefox is special.
+        window.WebSocket = window.MozWebSocket;
+    }
+
+    // Move websocket code to a jam module?
+    ws = new WebSocket("ws://localhost:8888");
+    ws.onopen = function() {
+        ws.send("Don't panic.");
+    };
+    ws.onmessage = function(event) {
+        alert("The server sent a message: " + event.data);
+    };
+
+    game = jam.Game(500, 300, document.body);
+    player = jam.Sprite(10, 100);
+    setInterval(
+        function(){
+            console.log(ws.readyState);
+        }, 100);
+};
